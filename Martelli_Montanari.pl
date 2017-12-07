4837a2a597528388d5a2ed8f13774d08393f1867
Doriane <doriane.lecam@live.com>
2017-12-07 20:14:09 +0100

    question 1 mais encore des problèmes ...

--- Martelli_Montanari.pl
+++ Martelli_Montanari.pl
@@ -19,8 +19,81 @@ echo(_).
 
 % predicat pour l application des regles
 
+% renommage d une variable
+% regles(x?=t,rename) :  true si t est une variable
+regles(E,rename) :- arg(1,E,X),arg(2,E,T),var(X), var(T),!.
 
+% simplification d une constante
+% regles(x?=a,simplify) :  true si a est une constante, si x est une constante, alors x==a
+regles(E,simplify) :- arg(1,E,X), arg(2,E,A), var(X), nonvar(A), ! ; arg(1,E,X), arg(2,E,A), nonvar(X),nonvar(A),X==A,!.
+
+% developpement
+% regles(x?=t,expand) : true si t est composé et x n apparait pas dans t
+regles(E,expand) :- arg(1,E,X), arg(2,E,T), var(X), compound(T), \+occur_check(X,T),!. 
  
- regles(E,decompose) :- arg(1,E,W),arg(2,E,X),compound(W),compound(X),!,functor(W,N,A),functor(X,M,B),N==M,A==B.
+% verification d occurence
+% regles(x?=t,check) : true si x\==t et x apparait dans t
+regles(E,check) :- arg(1,E,X), arg(2,E,T), var(X), X\==T, occur_check(X,T), !.
 
- regles(E,clash) :-  arg(1,E,W),arg(2,E,X),compound(W),compound(X),!,functor(W,N,A),functor(X,M,B),not( ( N==M , A==B ) ).
+% echange
+% regles(t?=x,orient) : true si t n est pas une variable
+regles(E,orient) :- arg(1,E,T), arg(2,E,X), var(X), nonvar(T), !.
+
+% decomposition de deux fonctions
+% regle(x?=t,decompose) : true si x et t ont le meme symbole ET la meme arite
+regles(E,decompose) :- arg(1,E,X),arg(2,E,T),functor(X,N,A),functor(T,M,B),N==M,A==B,!.
+
+% conflit entre deux fonctions
+% regle(x?=t,clash) : true si x et t n ont pas le meme symbole OU pas la meme arite
+regles(E,clash) :- arg(1,E,X),arg(2,E,T),functor(X,N,A),functor(T,M,B),not( ( N==M , A==B ) ),!.
+
+
+
+% test d occurence
+occur_check(V,T) :- var(V), arg(_,T,X), V==X, !.
+
+
+% application des regles
+% renommage
+% application(rename,x?=t,x?=t|p,q)  renomme les occurences de la variable x en la variable t
+application(rename,E,P,Q) :- arg(1,E,X), arg(2,E,T), X=T, Q=P.
+
+% simplification
+% application(simplify,x?=a,x?=a|p,q)  renomme les occurences de la variable x en la constante t
+application(simplify,E,P,Q) :- arg(1,E,X), arg(2,E,T), X=T, Q=P.
+
+% developpement
+% application(expand,x?f(v),x?=f(v)|p,q)  renomme les occurences de la variable x en terme compose f(v)
+application(expand,E,P,Q) :- arg(1,E,X), arg(2,E,T), X=T, Q=P.
+
+% echange
+% application(orient,t?=x,t?=x|p,q)  intervertis le t et le x
+application(orientdecompose,E,P,Q) :- arg(1,E,T), arg(2,E,X), append([X?=T],P,Q).
+
+% decomposition
+% application(decompose,f(x)?=f(y),f(x)?=f(y)|p,q)  decompose l equation
+application(decompose,E,P,Q) :- arg(1,E,X), arg(2,E,T), X=..XT, new_list(XT,XL), T=..TT, new_list(TT,TL), croisement(XL,TL,S), append(S,P,Q).
+
+
+
+% transforme le système d’équations P en le système d’équations Q par application de la règle de transformation R à l’équation E.
+% reduit(R,E,P,Q) : true si la regle est applicable sur l equation.
+reduit(R,E,P,Q) :- regles(E,R), !, write(R), application(R,E,P,Q).
+
+
+
+
+% fonctions complémentaires
+
+% new_list(A,B) : transforme une liste en une autre en ne gardant que le reste
+new_list([_|P],XL) :- XL=P.
+
+% croisement(A,B,C) : prend deux à deux les elements de chaque liste pour en faire une liste du type [A?=B, ... ]
+croisement([A|P],[B|Q],S) :- croisement(P,Q,Z), append([A?=B],Z,S).
+croisement([],[],S) :- S=[].
+
+
+unifie([A|P]) :- reduit(R,A,P,Q),!, unifie(Q). 
+unifie([]) :- write('Unification terminee').
+
+
